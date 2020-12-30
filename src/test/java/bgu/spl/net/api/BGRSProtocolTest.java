d0efac4505b70e8d6bef36c59dc5282131aac0c5
danam <dan.am5555555@gmail.com>
2020-12-30 20:19:45 +0200

    TODO

---
+++ src/test/java/bgu/spl/net/api/BGRSProtocolTest.java
@@ -0,0 +1,17 @@
+package bgu.spl.net.api;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BGRSProtocolTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void process() {
+    }
+}
