f3376c570336bb47d758d5b5955edb74c42f4c45
danam <dan.am5555555@gmail.com>
2020-12-30 20:21:41 +0200

    TODO

---
+++ src/main/java/bgu/spl/net/impl/BGRSServer/TPCMain.java
@@ -0,0 +1,19 @@
+package bgu.spl.net.impl.BGRSServer;
+
+import bgu.spl.net.api.BGRSEncoderDecoder;
+import bgu.spl.net.api.BGRSProtocol;
+import bgu.spl.net.srv.BaseServer;
+import bgu.spl.net.srv.BlockingConnectionHandler;
+import bgu.spl.net.srv.Server;
+
+public class TPCMain {
+    public static void main(String[] args) {
+        BaseServer TPCserver = new BaseServer(Integer.parseInt(args[0]), () -> new BGRSProtocol(), BGRSEncoderDecoder::new) {
+            @Override
+            protected void execute(BlockingConnectionHandler handler) {
+                new Thread(handler).start();
+            }
+        };
+        TPCserver.serve();
+    }
+}
