c917763933ebaf54e56b0d1a9b4c3514c658fd8d
danam <dan.am5555555@gmail.com>
2020-12-30 20:20:02 +0200

    TODO

---
+++ src/test/java/bgu/spl/net/srv/DatabaseTest.java
@@ -0,0 +1,58 @@
+package bgu.spl.net.srv;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void initialize() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void adminRegister() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void studentRegister() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void login() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void logout() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void courseReg() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void KDAMCheck() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void courseStat() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void studentStat() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isRegistered() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void unRegister() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMyCourses() {
+    }
+}
